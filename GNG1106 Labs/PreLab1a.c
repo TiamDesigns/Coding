9d17f827f3d70a2ac94942844e684a2b4294da2f
TiamDesigns <65985643+TiamDesigns@users.noreply.github.com>
2024-09-12 08:51:49 -0400

    PreLab Finished

---
+++ GNG1106 Labs/PreLab1a.c
@@ -0,0 +1,7 @@
+#include <stdio.h>// line 1
+int main()// line 2
+{// line 3
+   printf("Hello, World!\n");// line 4
+   return 0;// line 5
+}// line 6
+
---
+++ GNG1106 Labs/PreLab1b.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("VVV\n");
+    printf("VV\n");
+    printf("V\n");
+}
+
