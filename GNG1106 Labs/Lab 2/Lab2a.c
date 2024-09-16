83516224534ba66f06428d3f8657989dfa96547b
TiamDesigns <tiambennett@gmail.com>
2024-09-16 01:33:53 -0400

    Lab2A

--- GNG1106 Labs/Lab 2/Lab2a.c
+++ GNG1106 Labs/Lab 2/Lab2a.c
@@ -5,11 +5,10 @@ int length; //initialize variable for rectangle length
 
 int main()
 {
-    printf("Please enter the length of the rectangle in centimeter (Integer only)\n"); //prompt user to enter data for variable length
+    printf("Please enter the length then width of the rectangle in centimeters. (Integer only)\n"); //prompt user to enter data for variable length
+    //scanf("%d, %d", &length, &width); //scan for user input for variable length 
     scanf("%d", &length); //scan for user input for variable length 
-
-    printf("Please enter the width of the rectangle in centimeter (Integer only)\n"); //prompt user to enter data for variable width
-    scanf("%d", &width); //scan for user input for variable width
+    scanf("%d", &width); //scan for user input for variable length 
 
     printf("Ah, the length and width of the rectangle are %d ", length);
     printf("and %d respectively.\n", width);
