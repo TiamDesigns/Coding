5e3435c4f626c220549175434300f839465e256f
TiamDesigns <tiambennett@gmail.com>
2024-09-16 10:18:31 -0400

    Double type variables

--- GNG1106 Labs/Lab 2/Lab2c.c
+++ GNG1106 Labs/Lab 2/Lab2c.c
@@ -2,20 +2,21 @@
 
 //int width; //initialize variable for rectangle width
 //int length; //initialize variable for rectangle length
-float width, length; //float variables for length and width
+//float width, length; //float variables for length and width
+double width, length;
 
 int main()
 {
     printf("Please enter the length then width of the rectangle in centimeters. (Integer only)\n"); //prompt user to enter data for variable length
     //scanf("%d, %d", &length, &width); //scan for user input for variable length and width
-    scanf("%f", &length); //scan for user input for variable length 
-    scanf("%f", &width); //scan for user input for variable length 
+    scanf("%lf", &length); //scan for user input for variable length 
+    scanf("%lf", &width); //scan for user input for variable length 
 
-    printf("Ah, the length and width of the rectangle are %.1f ", length);
-    printf("and %.1f respectively.\n", width);
+    printf("Ah, the length and width of the rectangle are %.1lf ", length);
+    printf("and %.1lf respectively.\n", width);
 
     float perimeter = 2*(length+width); 
-    printf("The perimeter of the rectangle is %.1f.\n", perimeter);
+    printf("The perimeter of the rectangle is %.1lf.\n", perimeter);
     
     return 0;
 }
--- GNG1106 Labs/Lab 2/Lab2d.c
+++ GNG1106 Labs/Lab 2/Lab2d.c
@@ -2,25 +2,24 @@
 
 //int width; //initialize variable for rectangle width
 //int length; //initialize variable for rectangle length
-float width, length; //float variables for length and width
-
-
+//float width, length; //float variables for length and width
+double width, length; //double type variables for length and width
 
 int main()
 {
     printf("Please enter the length then width of the rectangle in centimeters. (Integer only)\n"); //prompt user to enter data for variable length
     //scanf("%d, %d", &length, &width); //scan for user input for variable length and width
-    scanf("%f", &length); //scan for user input for variable length 
-    scanf("%f", &width); //scan for user input for variable length 
+    scanf("%lf", &length); //scan for user input for variable length 
+    scanf("%lf", &width); //scan for user input for variable length 
 
-    printf("Ah, the length and width of the rectangle are %.1f ", length);
-    printf("and %.1f respectively.\n", width);
+    printf("Ah, the length and width of the rectangle are %.1lf ", length);
+    printf("and %.1lf respectively.\n", width);
 
     float perimeter = 2*(length+width); 
-    printf("The perimeter of the rectangle is %.1f centimeters.\n", perimeter);
+    printf("The perimeter of the rectangle is %.1lf centimeters.\n", perimeter);
 
     float area = length * width;
-    printf("The area of the rectangle is %.1f centimeters.\n", area);
+    printf("The area of the rectangle is %.1lf centimeters.\n", area);
 
     return 0;
 }
