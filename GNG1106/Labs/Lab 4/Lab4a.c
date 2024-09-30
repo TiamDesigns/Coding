40dd9ee9355d2584918818b76949249713137fa2
TiamDesigns <65985643+TiamDesigns@users.noreply.github.com>
2024-09-30 11:10:51 -0400

    Notation

--- GNG1106/Labs/Lab 4/Lab4a.c
+++ GNG1106/Labs/Lab 4/Lab4a.c
@@ -22,7 +22,7 @@ int main()
             break;
         case '/':
             if (b != 0)
-                printf("Result: %d\n", a / b);  // Integer division
+                printf("Result: %d\n", a / b);  
             else
                 printf("Error: Division by zero is not allowed.\n");
             break;
