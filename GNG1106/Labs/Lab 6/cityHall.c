aca60b498b32171c5fbbe0588434d0ffa1f13adc
TiamDesigns <tiambennett@gmail.com>
2024-10-21 02:10:37 -0400

    CityHallUpgraded

--- GNG1106/Labs/Lab 6/cityHall.c
+++ GNG1106/Labs/Lab 6/cityHall.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#define OPTION_LICENSE 5
+#define OPTION_LICENSE 1
 #define OPTION_STICKER 2
 #define OPTION_TICKET 3
 #define COUNTER_LICENSE 5
 #define COUNTER_STICKER 8
 #define COUNTER_TICKET 2
+#define ADMIN_MODE 12345
 
 int main()
 {
@@ -20,7 +21,7 @@ OPTION_LICENSE, OPTION_STICKER, OPTION_TICKET);
 		case OPTION_STICKER:
 			printf("Go to counter %d\n", COUNTER_STICKER);
                         break;
-                case OPTION_TICKET:               
+        case OPTION_TICKET:               
 			printf("Go to counter %d\n", COUNTER_TICKET);
                         break;
 		default:
